37e58d21a398167d160485618be866d578319728
Nima Karami <94768955+nima-karami@users.noreply.github.com>
2022-05-16 16:59:51 -0400

    adding css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Main from './Main';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="App">
+      <Sidebar />
+      <Main />
       notes app
     </div>
   );
---
+++ src/Sidebar.js
@@ -0,0 +1,26 @@
+function Sidebar() {
+    return <div className = 'app-sidebar'>
+        <div className = 'app-sidebar-header'>
+            <h1>Notes</h1>
+            <button>Add</button>
+        </div>
+
+        <div className = 'app-sidebar-notes'>
+            <div className = 'app-sidebar-note'>
+                <div className = 'sidebar-note-title'>
+                    <strong>Title</strong>
+                    <button>Delete</button>
+                </div>
+
+                <p>Note preview</p>
+
+                <small className = 'meta-data'>
+                    Last modified [date]
+                </small>
+
+            </div>
+        </div>
+    </div>
+}
+
+export default Sidebar;
